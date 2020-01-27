a3f5df7d1a6f474634ea72a1efc1bd930924754e
TarannumKhadri <tarannum@skaas.guru>
2020-01-27 16:22:19 +0530

    commiting exercise 4 unable to get output!console.log error

--- exercisefour.js
+++ exercisefour.js
@@ -1,32 +1,39 @@
-function Complex(real , imaginary) {
+class Complex {
+        constructor(real,imaginary){
         this.real =real;
         this.imaginary = imaginary;
 }
-function display(re,im){
+ display(re,im){
     if(im === '0') return '' + re;
     if(re === 0) return '' + im + 'i';
     if(im < 0) return '' + re + im + 'i';
     return '' + re + '+' + im + 'i';
 }
-function sum(num1,num2) {
+ sum() {
         var real = num1.real + num2.real;
         var imaginary = num1.imaginary + num2.imaginary;
         return display(real, imaginary);
 }
-function difference(num1,num2){
+ difference(){
         var real = num1.real - num2.real;
         var imaginary = num1.imaginary - num2.imaginary;
         return display(real, imaginary);
 }
-function product(num1,num2){
+product(){
         var real = (num1.real * num2.real)-(num1.imaginary * num2.imaginary);
         var imaginary = (num1.real * num2.imaginary)+(num1.imaginary * num2.real); 
         return display(real, imaginary);
 }
+get complex() { 
+    return this.sum()+ this.difference() + this.product();
+    
+}
+}
 var a = new Complex(prompt("real number: "),prompt("imaginary number: "));
 
 var b = new Complex(prompt("real part: "),prompt("imaginary part: "));
+//Complex.sum();
+//console.log(complex);
+//console.log(Complex.difference);
+//console.log(product(a,b));
 
-console.log(sum(a,b));
-console.log(difference(a,b));
-console.log(product(a,b));
