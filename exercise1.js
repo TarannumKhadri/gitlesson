b422de337f3dbfe6973dd88d19c288a9dfd5a857
TarannumKhadri <tarannum@skaas.guru>
2020-01-27 10:16:39 +0530

    commiting the first oops exercise

---
+++ exercise1.js
@@ -0,0 +1,10 @@
+var area={
+    setDim(){
+         length = window.prompt("Enter length: ");
+         breadth = window.prompt("enter breadth: ");
+    },
+    getArea(){
+        area = 2(length + breadth);
+    }
+}
+console.log("the area is :"+ area);
